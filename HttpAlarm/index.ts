b0a87676a87d792b210bd03e7fdb5b4e8c7eb298
Thomas <65050898+tvonment@users.noreply.github.com>
2022-05-03 20:29:43 +0000

    analyse

--- HttpAlarm/index.ts
+++ HttpAlarm/index.ts
@@ -1,15 +1,80 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import axios, { AxiosRequestConfig } from 'axios';
+
+const APP_ID = process.env["appId"];
+const APP_SECRET = process.env["appSecret"];
+const TENANT_ID = process.env["tenantId"];
+const TEAMS_WEBHOOK = process.env["teamsWebhook"]
+
+const TOKEN_ENDPOINT = 'https://login.microsoftonline.com/' + TENANT_ID + '/oauth2/v2.0/token';
+const MS_GRAPH_SCOPE = 'https://graph.microsoft.com/.default';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-
-    let text = "Hello World!"
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: text
-    };
+    context.log(req.body)
+    //await sendToTeams(req.body);
 
 };
-
 export default httpTrigger;
+
+async function sendToTeams(body: any) {
+    let config: AxiosRequestConfig = {
+        method: 'post',
+        url: TEAMS_WEBHOOK,
+        headers: {
+            'ContentType': 'Application/Json'
+        },
+        data: {
+            "type": "message",
+            "attachments": [
+                {
+                    "contentType": "application/vnd.microsoft.card.adaptive",
+                    "content": {
+                        "type": "AdaptiveCard",
+                        "body": [
+                            {
+                                "type": "ColumnSet",
+                                "columns": [
+                                    {
+                                        "type": "Column",
+                                        "width": "stretch",
+                                        "items": [
+                                            {
+                                                "type": "TextBlock",
+                                                "text": body.name,
+                                                "wrap": true,
+                                                "size": "Medium",
+                                                "weight": "Bolder"
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "type": "Column",
+                                        "width": "stretch",
+                                        "items": [
+                                            {
+                                                "type": "TextBlock",
+                                                "text": body.email,
+                                                "wrap": true,
+                                                "weight": "Lighter"
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "TextBlock",
+                                "text": "ALARM SEND!!",
+                                "wrap": true
+                            }
+                        ],
+                        "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                        "version": "1.3"
+                    }
+                }
+            ]
+        }
+    }
+    await axios(config);
+}
+
