1496faa9c79346aec1fd4c47689295070ca06855
thomas <thvome@gmail.com>
2023-07-05 10:34:47 +0200

    generalize monitoring

--- TimerHealthCheck/index.ts
+++ TimerHealthCheck/index.ts
@@ -1,7 +1,7 @@
 import { AzureFunction, Context } from "@azure/functions"
+import { Registry, Client } from "azure-iothub";
 
-const IOT_DEVICE_CONNECTION_STRING = process.env.IOT_DEVICE_CONNECTION_STRING
-const IOT_DEVICE_NAME = process.env.IOT_DEVICE_NAME
+const IOT_DEVICE_CONNECTION_STRING = process.env.IOTHUB_CONNECTION_STRING
 const IS_CONNECTED = process.env.IS_CONNECTED
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
@@ -20,24 +20,25 @@ const timerTrigger: AzureFunction = async function (context: Context, myTimer: a
         responseTimeoutInSeconds: 15 // set response timeout as 15 seconds
     };
 
-    const IotClient = require("azure-iothub").Client;
-    let iotClient = IotClient.fromConnectionString(IOT_DEVICE_CONNECTION_STRING);
-
-    iotClient.invokeDeviceMethod(IOT_DEVICE_NAME, methodParams, (err, result) => {
-        if (err) {
-            console.error('Failed to invoke method \'' + methodParams.methodName + '\': ' + err.message);
-            if (IS_CONNECTED === "true") {
-                appInsightsClient.trackEvent({ name: "CONNECTION LOST to " + IOT_DEVICE_NAME })
+    const registry = Registry.fromConnectionString(IOT_DEVICE_CONNECTION_STRING);
+    const client = Client.fromConnectionString(IOT_DEVICE_CONNECTION_STRING);
+    let devices = (await registry.list()).responseBody;
+    for (let d of devices) {
+        client.invokeDeviceMethod(d.deviceId, methodParams, (err, result) => {
+            if (err) {
+                console.error('Failed to invoke method \'' + methodParams.methodName + '\': ' + err.message);
+                if (IS_CONNECTED === "true") {
+                    appInsightsClient.trackEvent({ name: "CONNECTION LOST to " + d.deviceId })
+                } else {
+                    appInsightsClient.trackEvent({ name: "Disabled: " + d.deviceId })
+                }
             } else {
-                appInsightsClient.trackEvent({ name: "Disabled: " + IOT_DEVICE_NAME })
+                console.log(methodParams.methodName + ' on ' + d.deviceId + ':');
+                console.log(JSON.stringify(result, null, 2));
+                appInsightsClient.trackEvent({ name: 'successful connection to ' + d.deviceId })
             }
-        } else {
-            console.log(methodParams.methodName + ' on ' + IOT_DEVICE_NAME + ':');
-            console.log(JSON.stringify(result, null, 2));
-            appInsightsClient.trackEvent({ name: 'successful connection to ' + IOT_DEVICE_NAME })
-        }
-    });
-
+        });
+    }
     context.log('Timer trigger function ran!', timeStamp);
 };
 
